8ab03aa7756832e643d3941d0708bc8fc897f367
supernova37452 <sbush9@uic.edu>
2025-11-26 16:41:21 -0600

    i think this is a good set of  concepts to demo in one file

--- saja.rb
+++ saja.rb
@@ -1,1 +1,53 @@
 #i know youre gonna eat down fr AYYYEEE
+
+puts "typeeee"
+
+#sorry gang this is a bit long but this is actually really cool lmao i think we should demo method_missing + respond_to_missing + define_method together kinda like this example.
+# it shows 
+
+# whats interesting here? Ruby can modify its own classes at runtime!!
+# many static languages like Java, C#, C don't support "open classes" like this.
+# sure, they have reflection and runtime features, but Ruby lets you reopen and change classes as you go. 
+
+
+class Speaker
+    # if a method is not found, ruby will call this method 
+    # when you override "method_missing", you should override its corresponding "respond_to_missing" method
+    # why? because respond_to_missing is where you define how other objects can query
+    # whether THEY can handle that specific missing method or not
+    def method_missing(method_name, *args, &block)
+      if method_name.to_s.start_with?("shout_")
+        word_to_shout = method_name.to_s.sub("shout_", "")
+        # puts "#{word_to_shout.upcase}!! "
+        self.class.define_method(method_name) do
+            puts "#{word_to_shout.upcase}!!!"
+          end
+    
+          send(method_name) #call this method dynamically
+      else
+        super #a key word (similar to java when you use super to inherit from a parent class) means: “I’m not handling this missing method... go look at the parent classes method_missing. If they don't handle it, Ruby will raise the usual NoMethodError.”
+      end
+    end
+  
+    def respond_to_missing?(method_name, include_private = false)
+      method_name.to_s.start_with?("shout_") || super
+    end
+  end
+  
+  speaker = Speaker.new
+  speaker.shout_hello #first shouthello: this will go through -> call -> method_missing -> define method -> call the method      (missing -> method_missing -> define_method(:shout_hello) -> send(:shout_hello))
+  speaker.shout_hello #second shouthello: no missing → call the real method directly it exists now!
+  speaker.shout_ruby # the rest of these also go through the long way bc its out first encounter of these methods!
+  speaker.shout_metaprogramming
+  speaker.shout_this
+  speaker.shout_is
+  speaker.shout_cool
+  speaker.shout_sike
+
+  # important note: you actually modified the class at this point, not just that instance! so if you make a new one it'll now have shout_hello, shout_ruby, etc all as methods!
+  speaker2 = Speaker.new
+  speaker2.shout_hello 
+
+
+  
+
