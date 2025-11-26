2d9357c378fbd87c4abf73e2c12607cbbbdf8fb4
syhussein123 <147541072+syhussein123@users.noreply.github.com>
2025-11-25 21:02:27 -0600

    sorry I am committing again because like i needed you to see the explanation in that comment

--- demo.rb
+++ demo.rb
@@ -1,7 +1,9 @@
 # basic print statement: 
 puts "ayoooo helllooooooo"
 
-# basic class structure and definition with methods 
+#This code creates a new class, Numeric, with a plus method. Because everything is an object in Ruby, you can call this method with anything, even a number! We do this in the example y = 5.plus 6. The Numeric class is just a small example of what Ruby can do, but I'm sure it gives you an idea of its power.
+
+# that was just copy and paste from the source I got it from but like that is actually so cool 
 class Numeric
     def plus(x)
         self.+(x) # attached method that does initial.+ with x
