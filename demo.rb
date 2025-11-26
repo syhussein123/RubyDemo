3e2d2f52737e8528594351bb69bf7fbfd55d0fe0
syhussein123 <147541072+syhussein123@users.noreply.github.com>
2025-11-25 20:46:14 -0600

    omg printing with classes and functions and showing the difference

--- demo.rb
+++ demo.rb
@@ -1,1 +1,20 @@
-puts "ayoooo helloooooo"
+# basic print statement: 
+puts "ayoooo helllooooooo"
+
+# basic class structure and definition with methods 
+class Numeric
+    def plus(x)
+        self.+(x) # attached method that does initial.+ with x
+    end
+end
+
+# basic function definition
+def plus(x, y)
+    x + y
+end
+
+#omg they do the same thing 
+z = plus(3, 4) # does 3 + 4
+y = 5.plus(10) #does 5 + 10 
+puts y # prints 15
+puts z # prints 7
